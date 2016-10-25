5276fd24a7264b14323b6079e064b1f4449070da
David Twomey <ibanawx@gmail.com>
2016-10-25 14:42:59 -0400

    Define JSON formatter services

--- tests/integration/ContainerTest.php
+++ tests/integration/ContainerTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Ibanawx\Bundle\Prooph\EventStore\RestApiBundle\Tests\Integration;
 
+use Ibanawx\Bundle\Prooph\EventStore\RestApiBundle\Formatter\JsonStreamEventFormatter;
+use Ibanawx\Bundle\Prooph\EventStore\RestApiBundle\Formatter\JsonStreamFormatter;
 use Ibanawx\Bundle\Prooph\EventStore\RestApiBundle\Pagination\StreamPaginator;
 use Ibanawx\Bundle\Prooph\EventStore\RestApiBundle\Routing\StreamEventUriGenerator;
 use Ibanawx\Bundle\Prooph\EventStore\RestApiBundle\Routing\StreamUriGenerator;
@@ -33,6 +35,24 @@ class ContainerTest extends IntegrationTest
     {
         $service = $this->container->get('prooph_event_store_rest_api.stream_paginator');
         $this->assertInstanceOf(StreamPaginator::class, $service);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_the_json_stream_event_formatter_service()
+    {
+        $service = $this->container->get('prooph_event_store_rest_api.json_stream_event_formatter');
+        $this->assertInstanceOf(JsonStreamEventFormatter::class, $service);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_the_json_stream_formatter_service()
+    {
+        $service = $this->container->get('prooph_event_store_rest_api.json_stream_formatter');
+        $this->assertInstanceOf(JsonStreamFormatter::class, $service);
     }
 
 }
